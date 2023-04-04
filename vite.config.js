f14cc3873359f97eccdc340291fbb503448864f6
cartudev <cartcarll@gmail.com>
2023-04-04 12:33:41 -0300

    added contact page stylized, ionic-icons and some fixes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,16 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
     // config options
+    assetsInclude: ['./*.pdf'],
     base: './',
-  }
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          nested: resolve(__dirname, 'projects.html'),
+        },
+      },
+    },
+  })
